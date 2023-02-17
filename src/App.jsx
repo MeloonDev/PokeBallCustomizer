d31e9737f75512553a6d63cb4788f335635c0e00
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-02-18 00:47:38 +0100

    Add button on click effects and install leva

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { Environment, PresentationControls } from "@react-three/drei";
+import {
+  ContactShadows,
+  Environment,
+  PresentationControls,
+} from "@react-three/drei";
 import { Suspense } from "react";
 import { PokeBall } from "./PokeBall";
 
@@ -13,18 +17,26 @@ function App() {
         angle={0.15}
         penumbra={1}
         shadow-mapSize={[512, 512]}
+        shadow-normalBias={0.04}
         castShadow
       />
       <PresentationControls
         global
-        config={{ mass: 2, tension: 500 }}
-        snap={{ mass: 4, tension: 1500 }}
+        config={{ mass: 2, tension: 1000 }}
+        snap={{ mass: 4, tension: 500 }}
         rotation={[0, 0.3, 0]}
         polar={[-Math.PI / 3, Math.PI / 3]}
-        azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+        azimuth={[-Math.PI / 2, Math.PI / 3]}
       >
-        <PokeBall rotation={[-0.1, 0, 0.1]} />
+        <PokeBall rotation={[-0.1, 0, 0.1]} scale={0.8} />
       </PresentationControls>
+      <ContactShadows
+        position={[0, -1.4, 0]}
+        opacity={0.9}
+        scale={10}
+        blur={2}
+        far={4.5}
+      />
     </Suspense>
   );
 }
--- src/PokeBall.jsx
+++ src/PokeBall.jsx
@@ -1,24 +1,48 @@
 import { useGLTF } from "@react-three/drei";
+import { useControls } from "leva";
+import { useRef, useState } from "react";
 
 export function PokeBall(props) {
+  //leva
+  const TopColor = useControls({
+    r: 0.85,
+    g: 0.007,
+    b: 0.007,
+  });
+
+  //button clicker
+  const [buttonSound] = useState(() => new Audio("./sound/button.mp3"));
+  const button = useRef();
+  const buttonClickHandler = (e) => {
+    button.current.position.set(0, 0, -0.04);
+    buttonSound.currentTime = 0;
+    buttonSound.play();
+    setTimeout(() => {
+      button.current.position.set(0, 0, 0);
+    }, 100);
+  };
+
   const { nodes, materials } = useGLTF("./model/PokeBall.glb");
+  console.log(materials);
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Bottom.geometry}
-        material={materials.White}
+        material={materials.BottomMaterial}
         rotation={[Math.PI / 2, 0, 0]}
       />
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Inside.geometry}
-        material={materials.Black}
+        material={materials.InsideMaterial}
         rotation={[Math.PI / 2, 0, 0]}
       />
       <mesh
+        ref={button}
+        onClick={buttonClickHandler}
         castShadow
         receiveShadow
         geometry={nodes.Button.geometry}
@@ -29,14 +53,17 @@ export function PokeBall(props) {
         castShadow
         receiveShadow
         geometry={nodes.Emission.geometry}
-        material={nodes.Emission.material}
+        material={materials.EmissionMaterial}
         rotation={[Math.PI / 2, 0, 0]}
       />
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Top.geometry}
-        material={materials.Red}
+        material={materials.TopMaterial}
+        material-color-r={TopColor.r}
+        material-color-g={TopColor.g}
+        material-color-b={TopColor.b}
         rotation={[Math.PI / 2, 0, 0]}
       />
     </group>
