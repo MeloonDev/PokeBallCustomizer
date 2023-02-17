75471f9044b513be741f0e233ab122cd7daa2707
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-02-17 23:07:16 +0100

    Prepare scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Environment, PresentationControls } from "@react-three/drei";
+import { Suspense } from "react";
+
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <Suspense>
+      <color attach="background" args={["#2d4967"]} />
+      <Environment preset="city" />
+      <ambientLight intensity={0.5} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        shadow-mapSize={[512, 512]}
+        castShadow
+      />
+    </Suspense>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
     <App />
-  </React.StrictMode>,
-)
+  </Canvas>
+);
